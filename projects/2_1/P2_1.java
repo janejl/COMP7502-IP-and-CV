f03974a0c9ddc97c9d69ad7d0e7d514e3b3dd5fa
janejl <jane.hkucs@gmail.com>
2020-07-29 20:39:16 +0800

    add project2

---
+++ projects/2_1/P2_1.java
@@ -0,0 +1,24 @@
+/**
+  * In this task you will implement the method gradientImage of the class P2_1 which will calculate the gradient image.
+  *
+  * To determine the gradient in images in x and y directions use the masks [-1 0 1]^T and [-1 0 1], respectively.
+  *
+  * Note that values should be scaled to [0, 255]. This can be done by multiplying with 1 / sqrt(2).
+  *
+  * The solution files are provided for qualitative comparison. Output could be different because of differences in floating point arithmetic.
+  **/
+public class P2_1 {
+	public P2_1() {
+		Img img = new Img("Fig0314a.png");
+		gradientImage(img);
+		img.save();
+	}
+
+	public void gradientImage(Img i) {
+		//Your code here
+	}
+
+	public static void main(String[] args) {
+		new P2_1();
+	}
+}
