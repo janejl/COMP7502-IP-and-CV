12a62e541fbaab2abef7f7b82bd478a13858e6a7
janejl <jane.hkucs@gmail.com>
2020-08-11 14:13:24 +0800

    project2_1 done

--- projects/2_1/P2_1.java
+++ projects/2_1/P2_1.java
@@ -1,25 +1,43 @@
 /**
-  * In this task you will implement the method gradientImage of the class P2_1 which will calculate the gradient image.
-  *
-  * To determine the gradient in images in x and y directions use the masks [-1 0 1]^T and [-1 0 1], respectively.
-  *
-  * Note that values should be scaled to [0, 255]. This can be done by multiplying with 1 / sqrt(2).
-  *
-  * The solution files are provided for qualitative comparison. Output could be different because of differences in floating point arithmetic.
-  **/
+ * In this task you will implement the method gradientImage of the class P2_1 which will calculate the gradient image.
+ * <p>
+ * To determine the gradient in images in x and y directions use the masks [-1 0 1]^T and [-1 0 1], respectively.
+ * <p>
+ * Note that values should be scaled to [0, 255]. This can be done by multiplying with 1 / sqrt(2).
+ * <p>
+ * The solution files are provided for qualitative comparison. Output could be different because of differences in floating point arithmetic.
+ **/
 public class P2_1 {
-	public P2_1() {
-		Img img = new Img("Fig0314a.png");
-		gradientImage(img);
-		img.save();
-	}
+    public P2_1() {
+        Img img = new Img("Fig0314a.png");
+        gradientImage(img);
+        img.save();
+    }
 
-	public void gradientImage(Img i) {
-		//Your code here
-		// 2 1-d gaussian smoothing
-	}
+    public void gradientImage(Img i) {
+        //Your code here
+        // 2 1-d gaussian smoothing
+        byte[] imgCopy = new byte[i.img.length];
+        System.arraycopy(i.img, 0, imgCopy, 0, i.img.length);
 
-	public static void main(String[] args) {
-		new P2_1();
-	}
+        int a = 1;
+        int[] mask = new int[]{-1, 0, 1};
+        for (int x = a; x < i.height - a; x++) {
+            for (int y = a; y < i.width - a; y++) {
+                int fx = 0;
+                for (int s = -a; s <= a; s++) {
+                    fx += (int) (imgCopy[(x - s) * i.width + y] & 0xFF) * mask[a - s];
+                }
+                int fy = 0;
+                for (int v = -a; v <= a; v++) {
+                    fy += (int) (imgCopy[x * i.width + y - v] & 0xFF) * mask[a - v];
+                }
+                i.img[x * i.width + y] = (byte) (Math.sqrt(fx * fx + fy * fy) / Math.sqrt(2));
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        new P2_1();
+    }
 }
